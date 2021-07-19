fc768a19435b7e2cbd594a31ae28a3003eb4cf99
dkilfoyle <dkilfoyle@xtra.co.nz>
2021-07-19 22:21:15 +1200

    editor resize woes

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="codemirror1">My component</div>
+  <div
+    id="codemirror1"
+    ref="wrapperRef"
+    :style="{ height: wrapperHeight + 'px' }"
+  ></div>
 </template>
 
-<script>
-import { onMounted } from 'vue';
+<script lang="ts">
+import { onMounted, ref, toRefs, watchEffect, reactive } from 'vue';
 
 import { EditorState, EditorView, basicSetup } from '@codemirror/basic-setup';
 import { javascript } from '@codemirror/lang-javascript';
@@ -11,17 +15,40 @@ import { javascript } from '@codemirror/lang-javascript';
 export default {
   // name: 'ComponentName',
   setup() {
+    const state = reactive({
+      wrapperHeight: 10,
+    });
+
+    const wrapperRef = ref<HTMLElement>();
+
+    const onResize = (size: { height: number }) =>
+      (state.wrapperHeight = size.height);
+
     let startState = EditorState.create({
       doc: 'Hello World',
       extensions: [basicSetup, javascript()],
     });
     onMounted(() => {
+      debugger;
+      state.wrapperHeight =
+        wrapperRef?.value?.parentElement?.clientHeight || 111;
       let view = new EditorView({
         state: startState,
-        parent: document.getElementById('codemirror1'),
+        parent: document.getElementById('codemirror1') as Element,
       });
     });
-    return {};
+
+    return {
+      ...toRefs(state),
+      wrapperRef,
+      onResize,
+    };
   },
 };
 </script>
+
+<style>
+.cm-editor {
+  height: inherit;
+}
+</style>
